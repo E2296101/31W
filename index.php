4f1568de2a3eefe947d86574fcbaf74955e7aeb3
L'Hani <e2296101@cmaisonneuve.qc.ca>
2023-02-09 15:39:33 -0500

    Ajout de la fonction the_excerpt()

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ if (have_posts()):
     while (have_posts()) : the_post();
         the_title('<h1>','</h1>');
         the_content('<p>','</p>');
+        the_excerpt('<small>','</small>');
+        wp_trim_words( $text, $num_words, $more, $original_text )
     endwhile;
 endif;
 ?>   
