9ec5cb595bcdf1887d9ed88f76f62943175cce8a
L'Hani <e2296101@cmaisonneuve.qc.ca>
2023-02-09 16:27:07 -0500

    ajout des liens pour les articles, pas encore propre

--- index.php
+++ index.php
@@ -9,11 +9,14 @@ get_header() ?>
 if (have_posts()):
     while (have_posts()) : the_post();
         the_title('<h1>','</h1>');
-        the_content('<p>','</p>');
+        //the_content('<p>','</p>');
        // the_excerpt('<small>','</small>');
-       echo wp_trim_words(get_the_excerpt(),4);
+       $link = '<a href='.the_permalink().'>Lire</a>';
+       echo wp_trim_words(get_the_excerpt(),10),$link ;
     endwhile;
 endif;
 ?>   
 </main> 
 <?php get_footer(); ?>
+
+
