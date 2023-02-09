9755c04d7e185bd37348aa830cd243540e21c327
L'Hani <e2296101@cmaisonneuve.qc.ca>
2023-02-09 14:40:38 -0500

    ajout add_theme_support pour les titre et le logo au niveau de de fonction.php

--- functions.php
+++ functions.php
@@ -12,3 +12,10 @@ add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
 // La fonction add_theme_support('html5') est utilisée pour ajouter le support HTML5 pour les éléments de mise en forme dans un thème WordPress.
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
 
+// ajout add_theme_support pour les titre et le logo au niveau de de fonction.php
+add_theme_support( 'title-tag' );
+add_theme_support( 'custom-logo', array(
+    'height' => 480,
+    'width'  => 720,
+) );
+
