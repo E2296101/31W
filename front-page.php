60ed1745d1f83d1f73b5e3fd929f1a484d8d21f1
L'Hani <e2296101@cmaisonneuve.qc.ca>
2023-02-16 16:52:01 -0500

    ajout de _blocflex.scss, _markdown.scss , mise en forme des article en grille

--- front-page.php
+++ front-page.php
@@ -2,17 +2,23 @@
     get_header() 
 ?>
 <main>
-<?php 
-    if (have_posts()):
-        while (have_posts()) : the_post();
- ?>
-            <h4><?= get_the_title();  ?> </a></h4>
-<?php 
-            echo wp_trim_words(get_the_excerpt(), 40);
-            echo "<a href='".get_permalink()."'> Lire </a>";
-        endwhile;
-    endif;
-?>   
+<section class="blocflex">
+        <?php 
+            if (have_posts()):
+                while (have_posts()) : the_post();
+        ?>
+                <article>
+                    <h4><?= get_the_title();  ?> </a></h4>
+                <?php 
+                    echo wp_trim_words(get_the_excerpt(), 40);
+                    echo "<a href='".get_permalink()."'> Lire </a>"; 
+                ?>
+                </article>
+      <?php         
+            endwhile;
+        endif;
+        ?>  
+</section> 
 </main> 
 <?php 
     get_footer(); 
