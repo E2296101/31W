d88973a89754981c2211e0717415f35146eb5988
L'Hani <e2296101@cmaisonneuve.qc.ca>
2023-02-23 16:23:06 -0500

    Cours-8 : modification des parametres du  pour adapter l'affichage des catégories

--- category.php
+++ category.php
@@ -6,8 +6,17 @@
 <main>
 <section class="blocflex">
         <?php 
-            if (have_posts()):
-                while (have_posts()) : the_post();
+
+      $category = get_queried_object();
+      $args = array(
+         'category_name' => $category->slug,
+         'orderby' => 'title',
+         'order' => 'ASC'
+      );
+       $query = new WP_Query( $args );
+
+            if ($query->have_posts()):
+                while ($query->have_posts()) : $query->the_post();
         ?>
                 <article class="article-box-shadow">
                     <h2><?= get_the_title();  ?> </a></h2>
