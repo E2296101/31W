7c32b2559fcad36b9b48c2b03bf35522b4bda57a
L'Hani <e2296101@cmaisonneuve.qc.ca>
2023-03-02 15:56:23 -0500

    Cours-10 : ajout get_template_part() au niveau index.php

--- index.php
+++ index.php
@@ -6,17 +6,11 @@
 <main class="site__main">
     <section class="blocflex">
             <?php 
-                if (have_posts()):
-                    while (have_posts()) : the_post();
-            ?>
-                    <article class="article-box-shadow">
-                        <h2><?= get_the_title();  ?> </a></h2>
-                    <?php 
-                        echo wp_trim_words(get_the_excerpt(), 40);
-                        echo "<a href='".get_permalink()."'> Lire </a>"; 
-                    ?>
-                    </article>
-        <?php         
+            if (have_posts()):
+                while (have_posts()) : the_post();
+     
+                   get_template_part("template-parts/categorie", "note-cours");   
+          
                 endwhile;
             endif;
             ?>  
