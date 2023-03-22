88b03eb7937c1ae5ea8070b6a7f7a5f479f03f86
L'Hani <e2296101@cmaisonneuve.qc.ca>
2023-03-22 11:00:53 -0400

    Cours 15: ajout du code css et php pour afficher le champs personalisé en fonction de la categorie

--- single.php
+++ single.php
@@ -3,13 +3,16 @@
 ?>
 <main class="site__main">
 <?php 
-    echo"single.php";
     if (have_posts()):
     while(have_posts()): the_post();
             the_title('<h4>','</h4>');
             the_content('<p>','</p>');  
-        endwhile;    
+        endwhile;
+     if(in_category('cours')){  
+        get_template_part("template-parts/single", "cours"); 
+     }  
     endif;
+
 ?>   
 </main> 
 <?php get_footer(); ?>
---
+++ template-parts/single-cours.php
@@ -0,0 +1,10 @@
+
+<div class="info-article">
+    
+    <?php 
+    
+    echo "<p> Professeur : ".get_field( 'professeur' )."</p>";
+    echo "<p> Filiere : ".get_field( 'domaine' )."</p>";
+    // 22-03-2023 exo 3
+    ?>
+</div>
